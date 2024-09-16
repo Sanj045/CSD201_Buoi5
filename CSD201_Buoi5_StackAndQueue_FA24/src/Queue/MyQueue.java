b19430c7b5c0b9df71de698ea34c91f598affae3
Lương Bảo <142643063+Sanj045@users.noreply.github.com>
2024-09-16 17:49:32 +0700

    Add files via upload

---
+++ CSD201_Buoi5_StackAndQueue_FA24/src/Queue/MyQueue.java
@@ -0,0 +1,74 @@
+package Queue;
+
+public class MyQueue {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node front;
+    private Node rear;
+    private int length;
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+        length = 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void enqueue(int data) {
+        Node newNode = new Node(data);
+        // TH1 : Queue Null => front noi node dau tien
+        if (isEmpty()) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear.next = newNode;
+        }
+        rear = newNode;
+        length++;
+    }
+
+    // dequeue(): remove front element and return data of removed element
+    public int dequeue() {
+        if (isEmpty()) {
+            return -1; // or throw an exception
+        }
+        int data = front.data;
+        front = front.next;
+
+        // If the queue is empty after dequeue, set rear to null
+        if (front == null) {
+            rear = null;
+        }
+
+        length--;
+        return data;
+    }
+
+    // BTVN 1: 2 , 19 -> LeetCode
+    // BTVN 2: Implement Stack and Queue
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        System.out.println("Queue length: " + queue.getLength());
+    }
+
+}
